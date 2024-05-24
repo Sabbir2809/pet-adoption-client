38013f04b074103d898429965149a850e8bbf304
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2024-05-24 10:05:21 +0600

    (bugs): footer page

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -1,7 +1,6 @@
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import IconButton from "@mui/material/IconButton";
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -39,20 +38,17 @@ const Footer = () => {
             }}>
             <Box sx={{ width: { xs: "100%", sm: "60%" } }}>
               <Typography variant="h4" fontWeight={600} gutterBottom>
-                Pitus
+                Adoptify
               </Typography>
-
-              <Stack direction="row" justifyContent="left" spacing={1} useFlexGap>
-                <IconButton color="inherit" href="#" aria-label="GitHub">
-                  <FacebookIcon />
-                </IconButton>
-                <IconButton color="inherit" href="#" aria-label="X">
-                  <TwitterIcon />
-                </IconButton>
-                <IconButton color="inherit" href="#" aria-label="LinkedIn">
-                  <LinkedInIcon />
-                </IconButton>
-              </Stack>
+              <IconButton color="primary" aria-label="GitHub">
+                <FacebookIcon style={{ fontSize: "35px" }} />
+              </IconButton>
+              <IconButton color="primary" aria-label="X">
+                <TwitterIcon style={{ fontSize: "35px" }} />
+              </IconButton>
+              <IconButton color="primary" aria-label="LinkedIn">
+                <LinkedInIcon style={{ fontSize: "35px" }} />
+              </IconButton>
               <Typography variant="body2" color="text.secondary" mt={1}>
                 Copyright &#169; {new Date().getFullYear()} Pitus. All rights reserved.
               </Typography>
@@ -65,16 +61,16 @@ const Footer = () => {
               gap: 1,
             }}>
             <Typography variant="body2" fontWeight={600}>
-              Company
+              Explore
             </Typography>
-            <Link color="text.secondary" href="#">
+            <Link color="text.secondary" href="/about-us">
               About us
             </Link>
-            <Link color="text.secondary" href="#">
-              Careers
+            <Link color="text.secondary" href="/about-us">
+              Mission
             </Link>
-            <Link color="text.secondary" href="#">
-              Press
+            <Link color="text.secondary" href="/about-us">
+              Our Team
             </Link>
           </Box>
           <Box
@@ -91,9 +87,6 @@ const Footer = () => {
             </Link>
             <Link color="text.secondary" href="#">
               Privacy
-            </Link>
-            <Link color="text.secondary" href="#">
-              Contact
             </Link>
           </Box>
         </Box>
