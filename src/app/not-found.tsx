feec92c3924c6fabe4791bd7ece2e71eb9285120
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2024-05-24 11:13:03 +0600

    (bugs): not-found page design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,40 @@
+import { Box, Button, Typography } from "@mui/material";
 import Link from "next/link";
-import styles from "./NotFound.module.css";
 
-const notFoundPage = () => {
+const NotFoundPage = () => {
   return (
-    <div className={styles.section}>
-      <h1 className={styles.error}>404</h1>
-      <div className={styles.page}>Ooops!!! The page you are looking for is not found</div>
-      <Link className={styles.back_home} href="/">
-        Back to home
-      </Link>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        textAlign: "center",
+        backgroundImage: "linear-gradient(180deg, #CEE5FD, #FFF)",
+      }}>
+      <Typography variant="h1" sx={{ fontSize: "120px", fontWeight: "600", color: "primary.main" }}>
+        404
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ margin: "2rem 0", fontSize: "24px", fontWeight: 600, color: "primary.main" }}>
+        Ooops!!! The page you are looking for is not found
+      </Typography>
+      <Box sx={{ marginTop: "1rem" }}>
+        <Link href="/">
+          <Button
+            variant="contained"
+            sx={{
+              textTransform: "uppercase",
+              fontWeight: 600,
+            }}>
+            Back to Home
+          </Button>
+        </Link>
+      </Box>
+    </Box>
   );
 };
 
-export default notFoundPage;
+export default NotFoundPage;
